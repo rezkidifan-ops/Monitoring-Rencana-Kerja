d34dd9237bfcb94ee45b0aa8cbc8531a8606f904
rezkidifan-ops <rezkidifan@gmail.com>
2026-09-10 01:15:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from streamlit_gsheets import GSheetsConnection
 # 🔗 KONFIGURASI URL GOOGLE SHEET
 # Silakan ganti string di bawah ini dengan URL Google Sheet Anda
 # =========================================================
-SPREADSHEET_URL = "MASUKKAN_URL_GOOGLE_SHEET_ANDA_DI_SINI"
+SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1qJMHdTkURQV7LQE_DfO3UiX_txepHVCXqRct3mrQlxs/edit?usp=drivesdk"
 
 # 1. PENGATURAN HALAMAN
 st.set_page_config(
